675f3056f5514d66808d457bbf1c8fe3a5f75ead
tubbyK <menzies.chia@gmail.com>
2020-08-31 16:30:29 +0800

    Updated to use mute instead of mouse clicks as a safer action in preventing windows from locking/sleeping.

--- keep_alive.py
+++ keep_alive.py
@@ -1,56 +1,38 @@
-import pyautogui
 import time
 from pynput import keyboard
 
-class keep_alive():
-
-    def __init__(self, move=100, size_factor=0.8, wait_time=30):
-        self.x_pos, self.y_pos = None, None
-        self.get_mouse_pos()
-        self.move = move
-        self.width, self.height = None, None
-        self.get_screen_size()
-        self.size_factor = size_factor
+class keepAlive():
+    def __init__(self, wait_time=60):
+        self.keep_alive = False
         self.wait_time = wait_time
-        self.keep_going = True
 
-    def run(self):
+    def listen(self):
         listener = keyboard.Listener(on_release=self.on_release)
         listener.start()
-        while True:
-            if self.keep_going:
-                self.jitter()
 
     def on_release(self, key):
         if key == keyboard.Key.esc:
-            # Stop listener
-            self.keep_going = False if self.keep_going else True
-            print(f'Toggle: {self.keep_going}')
+            # toggle keep alive state
+            self.toggle_state()
 
-    def get_mouse_pos(self):
-        self.x_pos, self.y_pos = pyautogui.position()
+    def toggle_state(self):
+        self.keep_alive = not self.keep_alive
+        if self.keep_alive:
+            print('Keeping Windows from Locking')
+        else:
+            print('Letting Windows Lock')
 
-    def get_screen_size(self):
-        self.width, self.height = pyautogui.size()
-
-    def max_pos(self):
-        max_x = int(self.width * self.size_factor)
-        max_y = int(self.height * self.size_factor)
-        if self.x_pos > max_x:
-            self.x_pos = max_x
-        if self.y_pos > max_y:
-            self.y_pos = max_y
-
-    def jitter(self):
-        x_pos_original, y_pos_original = pyautogui.position()
-        self.x_pos, self.y_pos = pyautogui.position()
-        self.max_pos()
-        pyautogui.click(button='right')
-        pyautogui.moveTo(self.x_pos - 40, self.y_pos - 40)
-        pyautogui.click()
-        pyautogui.moveTo(x_pos_original, y_pos_original)
+    def do_action(self):
+        keyboard.Controller().press(keyboard.Key.media_volume_mute)
+        keyboard.Controller().press(keyboard.Key.media_volume_mute)
         time.sleep(self.wait_time)
 
+
 if __name__ == '__main__':
-    k = keep_alive()
-    k.run()
+    k = keepAlive()
+    k.listen()
+    k.toggle_state()
+
+    while True:
+        if k.keep_alive:
+            k.do_action()
